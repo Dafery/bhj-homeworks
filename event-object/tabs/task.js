d3c81d2c123ca55147de43177717c93b76ae6bbf
Maria Abramovich <mashkala2@gmail.com>
2022-12-04 20:56:32 +0300

    first commit tabs

---
+++ event-object/tabs/task.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const tabsWrappers = [...document.querySelectorAll('.tabs')];
+
+function switchingTabs(idx, tabs, tabsContent) {
+  return () => {
+    let activeIdx = tabs.findIndex((el) => el.classList.contains('tab_active'));
+
+    tabs[activeIdx].classList.remove('tab_active');
+    tabsContent[activeIdx].classList.remove('tab__content_active');
+
+    tabs[idx].classList.add('tab_active');
+    tabsContent[idx].classList.add('tab__content_active');
+  };
+}
+
+tabsWrappers.forEach((el) => {
+  const tabs = [...el.querySelectorAll('.tab')];
+  const tabsContent = [...el.querySelectorAll('.tab__content')];
+
+  tabs.forEach((el, idx) => el.addEventListener('click', switchingTabs(idx, tabs, tabsContent)));
+});
