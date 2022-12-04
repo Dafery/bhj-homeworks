9fa2ddce7472910744c94dc4940b061916b58ba1
Maria Abramovich <mashkala2@gmail.com>
2022-12-04 05:49:08 +0300

    first commit keysolo

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,13 +17,16 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода слова вызываем this.success()
-      При неправильном вводе символа - this.fail();
-     */
+    document.addEventListener('keyup', (e) => {
+      let currentSymbol = this.currentSymbol.textContent.toLowerCase();
+      let enteredSymbol = e.key.toLowerCase();
+      
+      if (enteredSymbol === currentSymbol) {
+        this.success();
+      } else {
+        this.fail();
+      }
+    });
   }
 
   success() {
